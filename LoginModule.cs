f489bdfa74fa6b331c34bc45a043dddc3ad7e8ad
cassidyb <cassidybollin32@gmail.com>
2016-10-08 17:44:46 -0700

    first_commit

---
+++ LoginModule.cs
@@ -0,0 +1,98 @@
+using Nancy;
+using System;
+using System.Collections.Generic;
+using DBConnection;
+using CryptoHelper;
+
+namespace login1
+{
+
+    public class LoginModule : NancyModule
+    {
+        public LoginModule()
+        {
+            Get("/", args =>
+            {
+                return View["index.sshtml"];
+            });
+
+            Post("/register", args =>
+            {
+                //show all the registered users
+                @ViewBag.showusers = "";
+                List<Dictionary<string, object>> myresults = DbConnector.ExecuteQuery("SELECT * FROM users");
+                myresults.Reverse();
+                foreach (Dictionary<string, object> item in myresults)
+                {
+                    @ViewBag.showusers += "<p>" + item["first_name"] + " " + "<br>" + "-" + item["last_name"] + " " + item["created_at"] + "</p>" + "<hr>";
+                }
+                //actually registering the user
+                string FirstName = Request.Form["first_name"];
+                string LastName = Request.Form["last_name"];
+                string Email = Request.Form["email"];
+                string Password = Request.Form["password"];
+                string Hash = Crypto.HashPassword(Password);
+                if(FirstName.Length > 2 && LastName.Length > 2 && Email.Length > 6 && Password.Length >= 8){
+                    //sending the information to our database
+                    string query = $"INSERT INTO users (first_name, last_name, email, hash, created_at) VALUES('{FirstName}', '{LastName}', '{Email}','{Hash}', NOW())";
+                    DbConnector.ExecuteQuery(query);
+                    Console.WriteLine("success");
+                    @ViewBag.error = false;
+                    //render the success page
+                    return Response.AsRedirect("/success");
+                }
+                Console.WriteLine("failed");
+                @ViewBag.error = true;
+                return View["index.sshtml"];
+
+            });
+            Post("/login", args =>
+            {
+                //show all the registered users
+                @ViewBag.showusers = "";
+                List<Dictionary<string, object>> myresults = DbConnector.ExecuteQuery("SELECT * FROM users");
+                myresults.Reverse();
+                foreach (Dictionary<string, object> item in myresults)
+                {
+                    @ViewBag.showusers += "<p>" + item["first_name"] + " " + "<br>" + "-" + item["last_name"] + " " + item["created_at"] + "</p>" + "<hr>";
+                }
+                //check the database for the entered information
+                string Email = Request.Form["email"];
+                string Password = Request.Form["password"];
+                string Hash = Crypto.HashPassword(Password);
+
+                @ViewBag.loggedin = "";
+
+                string query = $"GET user WHERE email = {Email} & WHERE hash = {Hash}";
+                DbConnector.ExecuteQuery(query);
+                // foreach (Dictionary<string, object> item in logtest)
+                // {
+                //     @ViewBag.loggedin += "<p>" + item["first_name"] + " " + "<br>" + "-" + item["last_name"] + " " + item["created_at"] + "</p>" + "<hr>";
+                // }
+
+                string User = Request.Form["email"];
+                Session["user"] = User;
+
+                return View["success.sshtml"];
+            });
+            //get route for whatever reason we may want to view registered users while logged into our session
+            Get("/success", args =>
+            {
+                @ViewBag.showusers = "";
+                List<Dictionary<string, object>> myresults = DbConnector.ExecuteQuery("SELECT * FROM users");
+                myresults.Reverse();
+                foreach (Dictionary<string, object> item in myresults)
+                {
+                    @ViewBag.showusers += "<p>" + item["first_name"] + " " + "<br>" + "-" + item["last_name"] + " " + item["created_at"] + "</p>" + "<hr>";
+                }
+
+                return View["success.sshtml", myresults];
+            });
+            Post("/clear", args =>
+            {
+               Session.DeleteAll();
+               return Response.AsRedirect("/");
+            });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System;
+using DBConnection;
+using CryptoHelper;
+
+
+namespace login1
+{
+    public class Program
+    {
+        public static void Create()
+        {
+        {
+            Console.WriteLine("Creating A New User");
+
+            Console.Write("Enter first name: ");
+            string FirstName = Console.ReadLine();
+
+            Console.Write("Enter last name: ");
+            string LastName = Console.ReadLine();
+
+            Console.Write("Enter email: ");
+            string Email = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            string Password = Console.ReadLine();
+            string Hash = Crypto.HashPassword(Password);
+
+            // Console.Write("Confirm password: ");
+            // string PasswordConfirm = Console.ReadLine();
+            // bool IsCorrectString = Crypto.VerifyHashedPassword(EncryptedString, UnencryptedString); // IsCorrectString == true
+
+            string query = $"INSERT INTO users (first_name, last_name, email, hash, created_at) VALUES('{FirstName}', '{LastName}', '{Email}', '{Hash}', NOW())";
+            DbConnector.ExecuteQuery(query);
+            Console.WriteLine($"{FirstName} has been added to the db!");
+        }
+        }
+
+        public static void Read()
+        {
+            // List<Dictionary<string, object>> myresults = DbConnector.ExecuteQuery("SELECT * FROM users");
+            // foreach (Dictionary<string, object> item in myresults)
+            // {
+            //     Console.WriteLine(item["id"] + " " + item["NAME"] + " " + item["Quote"] +  " " + item["created_at"]);
+            // }
+        }
+
+        public static void Update()
+        {
+            Console.WriteLine("Enter ID of user you wish to update:  ");
+            string userId = Console.ReadLine();
+            int user_id = Int32.Parse(userId);
+            Console.WriteLine("Update user's first name: ");
+            string NAME = Console.ReadLine();
+            Console.WriteLine("Update user's quote: ");
+            string Quote = Console.ReadLine();
+            string query = $"UPDATE users SET Name = '{NAME}', Quote = '{Quote}', updated_at = NOW() WHERE id = {user_id}";
+            DbConnector.ExecuteQuery(query);
+            Console.WriteLine($"{NAME}'s info has been updated!");
+        }
+
+        public static void Destroy()
+        {
+            Console.WriteLine("Enter ID of user you wish to delete:  ");
+            string userId = Console.ReadLine();
+            int user_id = Int32.Parse(userId);
+            string query = $"DELETE FROM users WHERE id = {user_id}";
+            DbConnector.ExecuteQuery(query);
+            Console.WriteLine($"A user has been deleted!");
+        }
+        public static void Main(string[] args)
+        {
+            // Create();
+            // Read();
+            // Update();
+            // Destroy();
+
+            IWebHost host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+        }
+    }
+}
